2fc79f2185fea32d9fc5afd7f121e70032cbf124
kayrongradilone <kayrongradilone@hotmail.com>
2023-03-14 22:13:12 -0300

    feat: finalizando about page e contact page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,84 +1,94 @@
-import Layout from '../Layout';
-import './index.scss';
-import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
-import Loader from 'react-loaders';
-
-
+import Layout from '../Layout'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    const Age = () => {
-        let myAge = new Date()
-        myAge = myAge.getFullYear() - 1994
-        return myAge
-    }
+  const Age = () => {
+    let myAge = new Date()
+    myAge = myAge.getFullYear() - 1994
+    return myAge
+  }
 
-    useEffect(() => {
-        hoverLetter()
-      },[])
+  useEffect(() => {
+    hoverLetter()
+  }, [])
 
+  const hoverLetter = () => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }
 
-    const hoverLetter = () => {
-        setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        },4000)
-      }
-    
-     
-
-    return (
-        <>
-        <Loader type='pacman'/>
-        <Layout/>
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                <AnimatedLetters
-                letterClass={letterClass}
-                strArray={['S','o','b','r','e',' ','m','i','m']}
-                idx={15}
-                />
-                </h1>
-                <p>Sou um desenvolvedor Front-End, com enfâse no usa da biblioteca React, atualmente estou me formando no cusro de Análise e desenvolvimento de sistemas e 
-                    estou a procura de ingressar na carreira.
-                </p>
-                <p>Tenho {Age()} anos, Natural de Santos-SP e morando em Florianópolis. Após 8 anos de trabalho no funcionalismo público, estou em busca de atuar na área que sempre foi aficionado, ao longo do meu curso de faculdade
-                   conheci e logo me apaixonei pelo Front-End, e comecei a estudar cada vez mais para me especializar nas melhores e mais atuais tecnologias da área.  
-                </p>
-                <p>Sou um profissional que busca sempre aprender com excelencia novas coisas, e evoluir na carreiro é sempre meu maior objetivo.</p>
-            </div>
-            <div className='stage-cube-cont'>
-            <div className='cubespinner'>
-
-                <div className='face2'>
-                    <FontAwesomeIcon icon={faAngular} color='#dd0031'/>
-                </div>
-                <div className='face1'>
-                    <FontAwesomeIcon icon={faReact} color='#5ed4f4'/>
-                </div>
-                <div className='face3'>
-                    <FontAwesomeIcon icon={faHtml5} color='#f06529'/>
-                </div>
-                <div className='face4'>
-                    <FontAwesomeIcon icon={faCss3} color='#28a4d9'/>
-                </div>
-                <div className='face5'>
-                    <FontAwesomeIcon icon={faJsSquare} color='#efd81d'/>
-                </div>
-                <div className='face6'>
-                    <FontAwesomeIcon icon={faGitAlt} color='#ec4d28'/>
-                </div>
-
-            </div>
+  return (
+    <>
+      <Loader type="pacman" />
+      <Layout />
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['S', 'o', 'b', 'r', 'e', ' ', 'm', 'i', 'm']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Sou um desenvolvedor Front-End, com enfâse no usa da biblioteca
+            React, atualmente estou me formando no cusro de Análise e
+            desenvolvimento de sistemas e estou a procura de ingressar na
+            carreira.
+          </p>
+          <p>
+            Tenho {Age()} anos, Natural de Santos-SP e morando em Florianópolis.
+            Após 8 anos de trabalho no funcionalismo público, estou em busca de
+            atuar na área que sempre fui aficionado, ao longo do meu curso de
+            faculdade conheci e logo me apaixonei pelo Front-End, e comecei a
+            estudar cada vez mais para me especializar nas melhores e mais
+            atuais tecnologias da área.
+          </p>
+          <p>
+            Sou um profissional que busca sempre aprender com excelencia novas
+            coisas, e evoluir na carreiro é sempre meu maior objetivo.
+          </p>
         </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face2">
+              <FontAwesomeIcon icon={faAngular} color="#dd0031" />
+            </div>
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+          </div>
         </div>
-        
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default About
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,142 @@
-import Loader from 'react-loaders';
-import Layout from '../Layout';
-import './index.scss';
+import Loader from 'react-loaders'
+import Layout from '../Layout'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect,  useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
-    return (
-        <>
-            <Layout/>
-            <Loader type='pacman'/>
-        </>
-    )
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const hoverLetter = () => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }
+
+  useEffect(() => {
+    hoverLetter()
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'gmailMessage',
+        'template_u85u39m',
+        e.target,
+        'Vo7Nbm1N4g1Wo9-Dz'
+      )
+      .then(
+        (result) => {
+          if (isLoading) {
+            setIsLoading(true)
+          }
+          alert('Email enviado com sucesso!!')
+          console.log(result.text)
+        },
+        (error) => {
+          alert('Erro ao enviar o Email, tente denovo!!')
+          console.log(error.text)
+        }
+      )
+  }
+
+  return (
+    <>
+      <Layout />
+      <Loader type="pacman" />
+
+      <div className="container contact-page">
+        <div className="info-map">
+          Kayron Gradilone,
+          <br />
+          Florianópolis, Brasil,
+          <br />
+          <span>kayrongradilone@hotmail.com</span>
+        </div>
+        <div className='map-wrap'>
+            <MapContainer center={[-27.5884043, -48.5476194 ]} zoom={13}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                <Marker position={[-27.5884043, -48.5476194]}>
+                    <Popup>Essa é minha localização!</Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[
+                'E',
+                'n',
+                't',
+                'r',
+                'e',
+                ' ',
+                'e',
+                'm',
+                ' ',
+                'c',
+                'o',
+                'n',
+                't',
+                'a',
+                't',
+                'o',
+              ]}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Estou interessado em conseguir meu primeiro trabalho como júnior em
+            desenvovimento, na parte do Front-End, fique a vontade para me
+            contatar e tirar qualquer dúvida sobre minhas habilidades.
+          </p>
+          <div className="contact-form">
+            <form onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Nome" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Assunto"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Mensagem"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+              </ul>
+              <input
+                type="submit"
+                className="flat-button"
+                value="Enviar"
+              ></input>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Contact
